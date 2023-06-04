65be0ab34c2a18fc671130199d26d2127c5ba206
Quoc Cuong <anhhungcodon2301@gmail.com>
2023-06-04 13:06:04 +0700

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -4,70 +4,66 @@ import {
     CardTitle
 } from 'reactstrap';
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
+function RenderDish({ dish }) {
 
-    renderDish(dish) {
-        if (dish == null) {
-            return (<div></div>);
-        }
+    if (dish == null) {
+        return (<div></div>);
+    }
+    return (
+        <div className='col-12 col-md-3'>
+            <Card>
+                <CardImg width='100%' src={dish.image} alt={dish.name} />
+                <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+    );
+
+}
+
+function RenderComments({ comments }) {
+
+    if (comments == null) {
+        return (<div></div>);
+    }
+    const showcmnts = comments.map((cmnt) => {
         return (
-            <div className='col-12 col-md-3'>
-                <Card>
-                    <CardImg width='100%' src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        );
-    }
-
-    renderComments(comments){
-        if(comments == null){
-            return(<div></div>);
-        }
-        const showcmnts = comments.map((cmnt) => {
-            return(
-                <li key={cmnt.id}>
-                    <p>{cmnt.comment}</p>
-                    <p>--{cmnt.author},
+            <li key={cmnt.id}>
+                <p>{cmnt.comment}</p>
+                <p>--{cmnt.author},
                     &nbsp;
-                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(cmnt.date)))}
-                    </p>
-                </li>
-            );
-        });
-
-        return (
-            <div className='col-12 col-md-5 m-1'>
-                <h4> Comments </h4>
-                <ul className='list-unstyled'>
-                    {showcmnts}
-                </ul>
-            </div>
+                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(cmnt.date)))}
+                </p>
+            </li>
         );
-    }
+    });
 
-    render() {
-        const dish = this.props.dish;
-        if (dish == null) {
-            return (<div></div>);
-        }
-        const dishItem = this.renderDish(dish);
-        const dishComments = this.renderComments(dish.comments);
+    return (
+        <div className='col-12 col-md-3'>
+            <h4> Comments </h4>
+            <ul className='list-unstyled'>
+                {showcmnts}
+            </ul>
+        </div>
+    );
+
+}
+
+
+const DishDetail = (props) => {
+
+    if (props.dish != null)
         return (
-            <div className="container">
+            <div className="container" key={props.dish}>
                 <div className='row'>
-                    {dishItem}
-                    {dishComments}
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.dish.comments} />
                 </div>
             </div>
         );
-    }
+
 }
 
 export default DishDetail;
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,52 +1,25 @@
-import { Component } from 'react';
-import {
-    Card, CardImg, CardImgOverlay, CardText, CardBody,
-    CardTitle
-} from 'reactstrap';
-import DishDetail from './DishdetailComponent';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay,
+    CardTitle } from 'reactstrap';
 
-class Menu extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedDish: null
-        }
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card
+                onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
     }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
-    }
+    const Menu = (props) => {
 
-    renderDish(dish) {
-        if (dish != null)
+        const menu = props.dishes.map((dish) => {
             return (
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        else
-            return (
-                <div></div>
-            );
-    }
-
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-                <div className="col-12 col-md-3">
-                    <Card key={dish.id}
-                        onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                <div className="col-12 col-md-3"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
                 </div>
             );
         });
@@ -56,10 +29,8 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <DishDetail selectedDish={this.state.selectedDish} />
             </div>
         );
     }
-}
 
 export default Menu;
